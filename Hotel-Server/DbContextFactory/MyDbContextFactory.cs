1494478cfd614758cb3cc7b264c84ce61620ecf8
jvarghe <services.box@outlook.com>
2018-04-30 21:27:16 -0400

    Re-wrote 'MyDbContextFactory.cs'. Added new information and removed some irrelevant data.

--- Hotel-Server/DbContextFactory/MyDbContextFactory.cs
+++ Hotel-Server/DbContextFactory/MyDbContextFactory.cs
@@ -1,15 +1,34 @@
 ﻿/* THE DATABASE CONTEXT FACTORY
  *
  * To talk to the database, Entity Framework Core needs to start a database session. The session 
- * comes in the form of a Context object. This file, MyDbContextFactory.cs, is a factory that 
- * creates such a database session object. At runtime, EFC will create a Context object which is 
- * fed the Data Model (from DbContext/Context.cs) and a database connection string (from this file). 
- * This object will be passed to Controllers in the AllController.cs file.
+ * comes in the form of a Context object. At runtime, EFC will create a Context object which is 
+ * fed the Data Model (from DbContext/Context.cs) and other configuration data (from this file).
+ *
+ * It is possible to completely perform this class's work in Context class [1], but I will do the 
+ * work of specifying connection strings, database providers etc., here in this file. This class,
+ * MyDbContextFactory, is a factory that creates such a database session object. This object will
+ * be passed to Controllers in the AllController.cs file. Any part of the program that wants to
+ * talk to the database will need this Context object.
+ *
+ * The first thing this class does is create a DbContextOptions object, which contains certain
+ * information that Context class needs[2]:
+ *
+ * 
+ *     * The database provider to use, typically selected by invoking a method such as
+ *       UseSqlServer or UseSqlite
+ *     * Any necessary connection string or identifier of the database instance, typically passed
+ *       as an argument to the provider selection method mentioned above
+ *     * Any provider-level optional behavior selectors, typically also chained inside the call
+ *       to the provider selection method
+ *     * Any general EF Core behavior selectors, typically chained after or before the provider
+ *       selector method
+ *
  * 
  * Copy and/or edit this file to direct Entity Framework to recognize other databases.
  * 
  * SOURCES
  * 1: https://docs.microsoft.com/en-us/ef/core/api/microsoft.entityframeworkcore.dbcontext
+ * 2: https://docs.microsoft.com/en-us/ef/core/miscellaneous/configuring-dbcontext
  */
 
 
@@ -20,31 +39,18 @@ using PostgresEFCore.Providers;
 
 namespace PostgresEFCore.Factories
 {
-    /// <summary>
-    /// "A DbContext instance represents a session with the database and can be used to query and 
-    /// save instances of your entities... Typically you create a class that derives from DbContext 
-    /// and contains DbSet<TEntity> properties for each entity in the model. If the DbSet<TEntity> 
-    /// properties have a public setter, they are automatically initialized when the instance of 
-    /// the derived context is created." [1]
-    /// </summary>
     public class MyDbContextFactory : IDesignTimeDbContextFactory<Context>
     {
         public Context CreateDbContext(string[] args)
         {
-            /* CONFIGURING THE DATABASE CONNECTION STRING
-             * 
-             * "Override the OnConfiguring(DbContextOptionsBuilder) method to configure the 
-             * database (and other options) to be used for the context. Alternatively, if you would 
-             * rather perform configuration externally instead of inline in your context, you can 
-             * use DbContextOptionsBuilder<TContext> (or DbContextOptionsBuilder) to externally 
-             * create an instance of DbContextOptions<TContext> (or DbContextOptions) and pass it 
-             * to a base constructor of DbContext." [1]
-             */
+            // INSTANTIATING AND CONFIGURING A DbContextOptions CLASS 
             var builder = new DbContextOptionsBuilder<Context>();
 
 
-            // For the sake of simplicity, we pass a hard-coded connection string to the Npgsql() 
-            // method to configure the database. You also could use dependency injection.
+            // We use the Npgsql() method to let EF Core know that we're using a PostgreSQL 
+            // database. For the sake of simplicity, we pass a hard-coded connection string to 
+            // the Npgsql() method to configure the database. You also could use dependency
+            // injection.
             builder.UseNpgsql("Host=localhost;" +
                               "Username=postgres;" +
                               "Password=password;" +
