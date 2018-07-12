2afba1cbe305269e7ee9b638978367b9b3708ff9
jvarghe <vcs.inbox@outlook.com>
2018-07-12 14:52:12 -0400

    Created the 'DatabasePopulator.cs' file and added to the Database folder.

---
+++ Hotel-Server/Database/DatabasePopulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Common.Models;
+
+
+namespace Hotel_Server.Database
+{
+    /// <summary>
+    /// This class checks whether a 'HotelReservationSystemDB' database exists and populates it.
+    /// The class checks if there are any hotels in the database, and if not, it assumes the
+    /// database is new and needs to be seeded with test data. It loads test data into arrays
+    /// rather than List<T> collections to optimize performance.
+    /// </summary>
+    public static class DatabasePopulator
+    {
+        
+    }
+}
