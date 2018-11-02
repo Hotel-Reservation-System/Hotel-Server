e3b5a48ac742ae27c28c44a87e90e2d1014bf6b0
jvarghe <vcs.inbox@outlook.com>
2018-11-02 18:32:20 -0400

    Edited the comments in 'MyDbContextFactory.cs'.

--- Hotel-Server/Data/MyDbContextFactory.cs
+++ Hotel-Server/Data/MyDbContextFactory.cs
@@ -1,18 +1,26 @@
 ﻿/* THE DATABASE CONTEXT FACTORY
  *
- * To talk to the database, Entity Framework Core needs to start a database session. The session 
- * comes in the form of a Context object. At runtime, EFC will create a Context object which is 
- * fed the Schema (from DbContext/Context.cs) and other configuration data (from this file).
+ * To talk to the database, Entity Framework Core needs to start a database session.
+ * The session comes in the form of a Context object. At runtime, EFC will create a
+ * Context object which is fed the Schema (from Data/Context.cs) and other configuration
+ * data (from this file).
  *
- * There are three possible ways of doing this; see Section 3.3 of Database Setup.txt. However, I
- * will do the work of specifying connection strings, database providers etc., here in this file.
- * This class, MyDbContextFactory, is a factory that creates such a database session object. This
- * object will be passed to Controllers in the AllController.cs file. Any part of the program that
- * wants to talk to the database will need this Context object.
+ * There are three possible ways of doing this; see 'Step 4: Configuring the Instantiation
+ * of the Context Object' in Context.cs for a full explanation. 
  *
- * The first thing this class does is create a DbContextOptions object, which contains certain
- * information that Context class needs. Copy and/or edit this file to direct Entity Framework to
- * recognize other databases.
+ * This class, MyDbContextFactory, is a factory that creates such a database session
+ * object. Connection string related values, such as host name, database providers,
+ * database login credentials etc. are defined here in this class in a
+ * DbContextOptionsBuilder<TContext> class. DbContextOptionsBuilder loads all this data
+ * into a DbContextOptions object, which in turn gets passed to a newly created Context
+ * object.
+ * 
+ * Once the Context session object has been created, it will made available to other
+ * services that need it. An example: Controller classes in the AllController.cs file.
+ * Any part of the program that wants to talk to the database will need this Context
+ * object.
+ *
+ * Copy and/or edit this file to direct Entity Framework to recognize other databases.
  *
  * 
  * SOURCES
@@ -32,18 +40,22 @@ namespace Hotel_Server.D
     {
         public Context CreateDbContext(string[] args)
         {
-            // INSTANTIATING AND CONFIGURING A DbContextOptions OBJECT
+            // INSTANTIATING AND CONFIGURING A DbContextOptionsBuilder OBJECT
             var builder = new DbContextOptionsBuilder<Context>();
 
 
-            // We use the Npgsql() method to let EF Core know that we're using a PostgreSQL 
-            // database. For the sake of simplicity, we pass a hard-coded connection string to 
-            // the Npgsql() method to configure the database. You also could use dependency
-            // injection.
+            // LOADING CONFIGURATION DATA INTO THE DbContextOptionsBuilder OBJECT
+            // We use the Npgsql() method to let EF Core know that we're using a
+            // PostgreSQL database. For the sake of simplicity, we pass a hard-coded
+            // connection string to the Npgsql() method to configure the database. 
             builder.UseNpgsql("Host=localhost;" +
                               "Username=postgres;" +
                               "Password=password;" +
                               "Database=HotelManagement");
+            
+            // CREATE A NEW CONTEXT OBJECT, INTO WHICH THE DbContextOptionsBuilder
+            // OBJECT WILL LOAD A DbContextOptions OBJECT THAT CONTAINS THE CONNECTION
+            // STRING
             return new Context(builder.Options);
         }
     }
